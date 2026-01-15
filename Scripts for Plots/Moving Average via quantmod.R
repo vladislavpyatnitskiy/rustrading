90a695c8af1a4563bcd85a760783d6d11c47695d
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-01-15 11:27:01 +0500

    Refactor data download and plotting logic

--- Scripts for Plots/Moving Average via quantmod.R
+++ Scripts for Plots/Moving Average via quantmod.R
@@ -2,26 +2,44 @@ lapply(c("quantmod", "timeSeries", "moexer"), require, character.only=T) # libs
 
 rus.ma.plt <- function(x, s = NULL, e = NULL, data = T){
   
-  if (isTRUE(data)){ p <- NULL # Data download loop for 4 scenarios:
+  if (data){ p <- NULL # Data download loop for 4 scenarios:
+    
+    getData <- function(A, s, e) { 
+      if (is.null(s) && is.null(e))
+        return(get_candles(A, from = "2007-07-20", interval = 'daily')) 
+      if (is.null(e)) return(get_candles(A, from = s, interval = 'daily')) 
+      if (is.null(s)) return(get_candles(A, till = e, interval = 'daily')) 
+      return(get_candles(A, from = s, till = e, interval = 'daily')) 
+    }
+    for (A in x){ D <- as.data.frame(getData(A, s, e)[,c(3,8)])
+    
+      message(
+        sprintf(
+          "%s is downloaded (%s / %s)", 
+          A, which(x == A), length(x)
+        )
+      ) # Download message
       
-    for (A in x){ D <- as.data.frame(get_candles(A, s, e,
-                                                 interval = 'daily')[,c(3,8)])
-    D <- D[!duplicated(D),] # Remove duplicates
+      D <- D[!duplicated(D),] # Remove duplicates
+      
+      p <- cbind(p, xts(D[, 1], order.by = as.Date(D[, 2]))) }
+      
+      p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Remove NA
+      
+      colnames(p) <- x
+      
+      x <- p } # Give tickers to data
+      
+    r <- as.timeSeries(x) # Make data time series
     
-    p <- cbind(p, xts(D[, 1], order.by = as.Date(D[, 2]))) }
-    
-    p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Remove NA
-    
-    colnames(p) <- x
-    
-    x <- p } # Give tickers to data
-    
-  r <- as.timeSeries(x) # Make data time series
-  
-  for (n in 1:ncol(r)){
-    
-    chartSeries(round(r[,n], 2), theme = "white",
-                name = sprintf("%s Stock Performance", colnames(r[,n])),
-                TA = "addEMA(50, col='purple');addEMA(200, col='red')") }
+    for (n in 1:ncol(r)){ # Plot
+      
+      chartSeries(
+        round(r[,n], 2), 
+        theme = "white",
+        name = sprintf("%s Stock Performance", colnames(r[,n])),
+        TA = "addEMA(50, col='purple');addEMA(200, col='red')"
+      ) 
+      }
 }
 rus.ma.plt(x = "LKOH", s = "2022-01-01", data = T)
